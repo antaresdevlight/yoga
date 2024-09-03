678b1b41c83d62bce3195a3ba205cfa79c63c102
antaresdevlight <antares.dev.lab@gmail.com>
2024-09-02 18:35:03 -0600

    base

---
+++ src/components/ButtonExample/index.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@chakra-ui/react";
+
+function ButtonExample() {
+  return (
+    <Button
+      variant="unstyled"
+      width="200px"
+      height="40px"
+      backgroundColor="#688E26"
+      color="white"
+      borderRadius="10px"
+    >
+      Haz Click
+    </Button>
+  );
+}
+
+export default ButtonExample;
