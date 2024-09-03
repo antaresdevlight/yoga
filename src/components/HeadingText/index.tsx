dc9d691e89f429add8ad1d35537a7f03c66e87be
luisaDev14 <malu.23.dev@gmail.com>
2024-09-02 18:33:20 -0600

    texts

---
+++ src/components/HeadingText/index.tsx
@@ -0,0 +1,11 @@
+import { Flex,Text } from "@chakra-ui/react";
+
+function HeadingText() {
+    return (
+        <Flex border="1px" justifyContent="center">
+            <Text textAlign="center" color="#112734" border="1px" fontSize="64px" fontWeight="400" lineHeight="1">Member Benefits</Text>
+        </Flex>
+    )
+}
+
+export default HeadingText
