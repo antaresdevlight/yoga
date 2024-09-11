bc8f28ea35c84764e479f52eff4fb3dbe5552267
luisaDev14 <malu.23.dev@gmail.com>
2024-09-10 18:20:29 -0600

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,38 @@
 import { Flex, Text, Button } from "@chakra-ui/react";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
 
 function Footer() {
-    return (
-        <Flex direction="column" border="1px">
-            <Text fontSize="64px">Footer</Text>
+  return (
+    <Flex
+      w="100%"
+      border="1px"
+      justifyContent="center"
+      direction="column"
+      alignItems="center"
+      paddingBottom="30px"
+      paddingTop="30px"
+      backgroundColor="aqua"
+    >
+      <Flex gap="20px">
+        <Text fontSize="18px">Home</Text>
+        <Text fontSize="18px">Experience</Text>
+        <Text fontSize="18px">News</Text>
+        <Text fontSize="18px">Experience</Text>
+        <Text fontSize="18px">Experience</Text>
+        <Text fontSize="18px">Experience</Text>
+      </Flex>
 
-            <Flex gap="10px">
-                <Button width="250px">Haz click</Button>
-                <Button width="250px">Haz click 2</Button>
-            </Flex>
-            
-        </Flex>
-    )
+      <Flex gap="20px" marginTop="20px" marginBottom="20px">
+        <FaFacebook style={{ width: "30px", height: "30px" }} />
+        <FaInstagramSquare style={{ width: "30px", height: "30px" }} />
+      </Flex>
+
+      <Flex>
+        <Text fontSize="16px">Copy 2024</Text>
+      </Flex>
+    </Flex>
+  );
 }
 
-export default Footer
+export default Footer;
