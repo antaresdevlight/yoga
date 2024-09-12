6957fce007dca8bee04e1a3ec0e2479e2123cb3c
luisaDev14 <malu.23.dev@gmail.com>
2024-09-11 18:00:16 -0600

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,20 @@
-import { Flex, Text } from "@chakra-ui/react";
-
-const styles = {
-  header: {
-    w: "100%",
-    h: "90px",
-    bg: "blue.200",
-  },
-};
+import { Flex, Link, Text } from "@chakra-ui/react";
 
 function Header() {
   return (
-    <Flex {...styles.header}>
-      <Text>Header</Text>
+    <Flex paddingBottom="50px" paddingTop="50px" justifyContent="center" gap="30px"alignItems="center">
+      <Flex justifyContent="center" gap="10px"alignItems="center">
+        <Flex border="2px" w="45px" h="45px"></Flex>
+        <Text fontSize="18px" fontWeight="700" lineHeight="normal" color="#375B26">Yoga International</Text>
+      </Flex>
+
+      <Flex  gap="30px">
+        <Link fontSize="14px" fontWeight="700" lineHeight="normal" color="#112734">Articles</Link>
+        <Link fontSize="14px" fontWeight="700" lineHeight="normal" color="#112734">Classes</Link>
+        <Link fontSize="14px" fontWeight="700" lineHeight="normal" color="#112734">Courses</Link>
+        <Link fontSize="14px" fontWeight="700" lineHeight="normal" color="#112734">Teachers</Link>
+        <Link fontSize="14px" fontWeight="700" lineHeight="normal" color="#112734">About</Link>
+      </Flex>
     </Flex>
   );
 }
